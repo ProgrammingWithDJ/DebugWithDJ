f3a9919ecd404957b6b67ec8caddb974e0e2d7df
Dipesh Jadhav <dipxsss@gmail.com>
2025-12-31 11:59:37 +0530

    Update youtube

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -3,13 +3,22 @@ import { RevealOnScroll } from "../RevealOnScroll";
 export const About = () => {
   const frontendSkills = [
     "React",
-    "Vue",
+    "Angular",
     "TypeScript",
-    "TailwindCSS",
-    "Svelte",
+    "TailwindCSS"
   ];
 
-  const backendSkills = ["Node.js", "Python", "AWS", "MongoDB", "GraphQL"];
+  const otherSkills = [
+    "oAuth2.0",
+    "openID connect",
+    "RESTful APIs",
+    "Git & GitHub",
+    "Graph API",
+    "Postman",
+    "JWT",
+  ];
+
+  const backendSkills = ["Dot Net Core", "SQL Server", "Azure"];
 
   return (
     <section
@@ -47,6 +56,22 @@ export const About = () => {
               </div>
 
               <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4"> Tools</h3>
+                <div className="flex flex-wrap gap-2">
+                  {otherSkills.map((tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition
+                    "
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+              </div>
+
+              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
                 <h3 className="text-xl font-bold mb-4"> Backend</h3>
                 <div className="flex flex-wrap gap-2">
                   {backendSkills.map((tech, key) => (
@@ -69,12 +94,27 @@ export const About = () => {
               <h3 className="text-xl font-bold mb-4"> 🏫 Education </h3>
               <ul className="list-disc list-inside text-gray-300 space-y-2">
                 <li>
-                  <strong> B.S. in Computer Science </strong> - XYZ University
-                  (2016-2020)
+                  <strong> Masters in Computer Science </strong> Mumbai University
+                  (2013-2018)
                 </li>
                 <li>
-                  Relevant Coursework: Data Structures, Web Development, Cloud
-                  Computing...
+                  <a href="https://www.youtube.com/watch?v=knD4n4HrIj4">Smart Web Browser</a>
+                  - Developed a secure and efficient web browser using C# and
+                  .NET Framework, featuring tabbed browsing, bookmarks, and a
+                  user-friendly interface.
+                </li>
+                <li>
+                  <a href="https://www.youtube.com/watch?v=aqvtWKQsuFM">Smart Helmet</a>
+                  - Created an IoT-based smart helmet using Rasberry Pi to enhance
+                  rider safety with features like accident detection and GPS
+                  tracking.
+                </li>
+
+                <li>
+                  <a href="https://www.youtube.com/watch?v=ZrKun1T7lX0">Covid Tracker</a>
+                  - Developed a Covid-19 tracking application using Angular and
+                  Postman API to provide real-time updates on infection rates and
+                  safety guidelines.
                 </li>
               </ul>
             </div>
@@ -84,23 +124,34 @@ export const About = () => {
                 <div>
                   <h4 className="font-semibold">
                     {" "}
-                    Software Engineer at ABC Corp (2020 - Present){" "}
+                    Senior Consultant at Plante Moran (2023 - Present){" "}
                   </h4>
                   <p>
-                    Developed and maintained microservices for cloud-based
-                    applications.
+                   
+  <li>Lead the team of 4 developers to build and maintain web applications using .NET Core API, sql server, logic apps</li>
+  <li> Commincated with clients to gather requirements and provide technical solutions.</li>
+  <li>planned the project roadmap and ensured timely delivery of milestones.</li>
+  <li>Mentored junior developers and conducted code reviews to maintain code quality.</li>
+  <li> Collaborated with cross-functional teams including designers, product managers, and QA engineers to deliver high-quality software solutions.</li>
+  <li> Delivered multiple successful project deployments, resulting in a 20% increase in client satisfaction.</li>
+
+                   
+
                   </p>
                 </div>
 
                 <div>
                   <h4 className="font-semibold">
                     {" "}
-                    Intern at DEF Startups (2019){" "}
+                    Developer Consultant at Microsoft (2021-2023){" "}
                   </h4>
                   <p>
-                    Assisted in building front-end components and integration
-                    REST APIs
+                    Expert Consultant in Microsoft Graph API.
                   </p>
+                   <h4 className="font-semibold">
+                    {" "}
+                    Atos Syntel (2018-2021){" "}
+                  </h4>
                 </div>
               </div>
             </div>
--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -10,17 +10,17 @@ export const Projects = () => {
         <div className="max-w-5xl mx-auto px-4">
           <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
             {" "}
-            Featured Projects
+            Youtube Channel
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-              <h3 className="text-xl font-bold mb-2"> Cloud Platform</h3>
+              <h3 className="text-xl font-bold mb-2">Automate Azure AD</h3>
               <p className="text-gray-400 mb-4">
-                Scalable cloud infrastructure management with real-time
-                monitoring and automated scaling.
+               <iframe width="450" height="450" src="https://www.youtube.com/embed/iYLXgZazZXU?si=Pfydln3PHi2FnMI_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin"></iframe>
+                             Use PowerShell to automate Azure AD user and group management.
               </p>
               <div className="flex flex-wrap gap-2 mb-4">
-                {["React", "Node.js", "AWS", "Docker"].map((tech, key) => (
+                {["AzureAD", "Graph API", "Postman"].map((tech, key) => (
                   <span
                     key={key}
                     className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
@@ -49,13 +49,14 @@ export const Projects = () => {
               transition-all
             "
             >
-              <h3 className="text-xl font-bold mb-2">AI Analytics Dashboard</h3>
+              <h3 className="text-xl font-bold mb-2">Expiring Secrets</h3>
               <p className="text-gray-400 mb-4">
-                ML-powered data visualization platform with predictive analytics
-                and interactive reports.
+                <iframe width="450" height="450" src="https://www.youtube.com/embed/njNYUXWsm3s?si=tuxf36HqooI9IYxq" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin"></iframe>
+                Use Azure AD and Graph API to monitor and notify about expiring
+                secrets.
               </p>
               <div className="flex flex-wrap gap-2 mb-4">
-                {["Python", "TensorFlow", "D3.js", "Flask"].map((tech, key) => (
+                {["Graph API", "Logic Apps", "Azure AD"].map((tech, key) => (
                   <span
                     key={key}
                     className="
@@ -88,17 +89,16 @@ export const Projects = () => {
               transition-all
             "
             >
-              <h3 className="text-xl font-bold mb-2">E-Commerce Web App</h3>
+              <h3 className="text-xl font-bold mb-2">Webhooks using funtion app</h3>
               <p className="text-gray-400 mb-4">
-                Full-stack e-commerce with modern UI, secure payment
-                integration, and customizable product inventory.
+                <iframe width="450" height="450" src="https://www.youtube.com/embed/NlxLcEnYT0o?si=74F0-Ux-J8GRZvr4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" ></iframe>
+using Azure Function App to create webhooks for real-time data processing with graph api
               </p>
               <div className="flex flex-wrap gap-2 mb-4">
-                {["Next.js", "TypeScript", "Stripe", "PostgreSQL"].map(
-                  (tech) => (
-                    <span
-                      key={tech}
-                      className="
+                {["Graph API", "Function Apps", "Azure AD"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="
                       bg-blue-500/10 text-blue-500 py-1 px-3 
                       rounded-full text-sm
                       transition
@@ -129,13 +129,13 @@ export const Projects = () => {
               transition-all
             "
             >
-              <h3 className="text-xl font-bold mb-2">Real-Time Chat App</h3>
+              <h3 className="text-xl font-bold mb-2">Certificate-Based Auth</h3>
               <p className="text-gray-400 mb-4">
-                Scalable chat platform supporting real-time messaging, presence,
-                and group chat features.
+                <iframe width="450" height="450" src="https://www.youtube.com/embed/7Bkv9J9mVr0?si=waur3Psn8SFb7Qww" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin"></iframe>
+                Implement certificate-based authentication with Azure AD and Graph API.
               </p>
               <div className="flex flex-wrap gap-2 mb-4">
-                {["Socket.IO", "Express", "React", "Redis"].map((tech, key) => (
+                {["Certificate-Based Auth", "Azure AD", "Graph API"].map((tech, key) => (
                   <span
                     key={key}
                     className="
