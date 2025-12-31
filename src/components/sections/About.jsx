5d6d6237f88c7faa44774b37d2fba0dad532890e
Dipesh Jadhav <dipxsss@gmail.com>
2025-12-31 12:07:41 +0530

    Update skills

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -18,6 +18,13 @@ export const About = () => {
     "JWT",
   ];
 
+  const azureSkills = [
+    "Azure AD",
+    "Logic Apps",
+    "Azure Functions",
+    "Azure App Services",
+  ];
+
   const backendSkills = ["Dot Net Core", "SQL Server", "Azure"];
 
   return (
@@ -43,6 +50,21 @@ export const About = () => {
                 <h3 className="text-xl font-bold mb-4"> Frontend</h3>
                 <div className="flex flex-wrap gap-2">
                   {frontendSkills.map((tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition
+                    "
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+              </div>
+               <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4"> Azure Skills</h3>
+                <div className="flex flex-wrap gap-2">
+                  {azureSkills.map((tech, key) => (
                     <span
                       key={key}
                       className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
